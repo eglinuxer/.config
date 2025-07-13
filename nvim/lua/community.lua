cff8d74cc68a6a80a77c8ff78b6ca09142e40f52
chaojun.xiao <chaojun.xiao@mthreads.com>
2025-07-13 22:17:00 +0800

    add: add support for tmux work with nvim.
    Signed-off-by: chaojun.xiao <chaojun.xiao@mthreads.com>

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -6,5 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
   -- import/override with your plugins folder
 }
